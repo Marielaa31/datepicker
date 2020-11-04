6780ef804fc361e322603d7cad483ddf6debd149
Marielaa31 <69422705+Marielaa31@users.noreply.github.com>
2020-11-04 23:26:38 +0800

    updated code

--- app/src/main/java/com/laundry/myapplication/MainActivity.java
+++ app/src/main/java/com/laundry/myapplication/MainActivity.java
@@ -1,29 +1,27 @@
 package com.laundry.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-
 import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.Calendar;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
     static TextView FirstDate;
     static TextView SecondDate;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,78 +30,85 @@ public class MainActivity extends AppCompatActivity {
         FirstDate=findViewById(R.id.FirstDate);
         SecondDate=findViewById(R.id.SecondDate);
 
-        FirstDate.setOnClickListener(new View.OnClickListener() {
+
+        FirstDate.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View view) {
-                showTruitonDatePickerDialog(view);
+            public void onClick(View v) {
+
+                Calendar c= Calendar.getInstance();
+
+                int year1= c.get(Calendar.YEAR);
+                int month1= c.get(Calendar.MONTH);
+                int day1= c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog=new DatePickerDialog(
+                        MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                        int monthreal=month+1;
+                        String firstdate=dayOfMonth+"/"+monthreal+"/"+year;
+
+                        FirstDate.setText(firstdate);
+                    }
+                },year1,month1,day1
+                );
+                datePickerDialog.getDatePicker().setMinDate(c.getTimeInMillis());
+                datePickerDialog.show();
+            }
+        });
+
+
+        SecondDate.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirstDateSelected();
 
             }
         });
 
 
-        SecondDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showToDatePickerDialog(view);
-            }
-        });
-    }
-    public void showTruitonDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
-    }
-
-    public void showToDatePickerDialog(View v) {
-        DialogFragment newFragment = new ToDatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
-    }
-
-    public static class DatePickerFragment extends DialogFragment implements
-            DatePickerDialog.OnDateSetListener {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current date as the default date in the picker
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-            DatePickerDialog datePickerDialog;
-            datePickerDialog = new DatePickerDialog(getActivity(),this, year,
-                    month,day);
-            c.set(year,month,day+1);
-            datePickerDialog.getDatePicker().setMinDate(c.getTimeInMillis());
-            return datePickerDialog;
-        }
-
-        public void onDateSet(DatePicker view, int year, int month, int day) {
-            // Do something with the date chosen by the user
-            FirstDate.setText(day + "/" + (month+1)  + "/" + year);
-        }
 
     }
+    public Boolean FirstDateSelected(){
+        String getfromdate = FirstDate.getText().toString().trim();
+        // String getfrom[] = getfromdate.split("/");
+        String datetrim=FirstDate.getText().toString().replaceAll("/","");
 
-    public static class ToDatePickerFragment extends DialogFragment implements
-            DatePickerDialog.OnDateSetListener {
-        // Calendar startDateCalendar=Calendar.getInstance();
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current date as the default date in the picker
-            String getfromdate = FirstDate.getText().toString().trim();
+        if (datetrim.matches("[0-9]+")){
+            //String getfromdate = FirstDate.getText().toString().trim();
             String getfrom[] = getfromdate.split("/");
+            //String datetrim=FirstDate.getText().toString().replaceAll("/","");
+            Log.d("num",datetrim);
             int year,month,day;
             year= Integer.parseInt(getfrom[2]);
             month = Integer.parseInt(getfrom[1])-1;
             day = Integer.parseInt(getfrom[0]);
             final Calendar c = Calendar.getInstance();
             c.set(year,month,day+1);
-            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),this, year,month,day);
-            datePickerDialog.getDatePicker().setMinDate(c.getTimeInMillis());
-            return datePickerDialog;
-        }
-        public void onDateSet(DatePicker view, int year, int month, int day) {
+            DatePickerDialog datePickerDialog=new DatePickerDialog(
+                    MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
-            SecondDate.setText(day + "/" + (month+1)  + "/" + year);
+                    int monthreal=month+1;
+                    String firstdate=dayOfMonth+"/"+monthreal+"/"+year;
+
+                    SecondDate.setText(firstdate);
+                }
+            },year,month,day
+            );
+            datePickerDialog.getDatePicker().setMinDate(c.getTimeInMillis());
+            datePickerDialog.show();
+            return true;
         }
+        else{
+            Toast.makeText(MainActivity.this,"Please select the first date",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+
     }
+
 
 }
